1e4f58de69e4c6aea64db2f0312f102ed0ff86bd
Garrett Wesley <garrettwesley@berkeley.edu>
2017-09-26 17:17:51 -0700

    Complete hw2

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,5 +1,10 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
   def initialize(baz)
+    @baz = baz
+  end
+
+  def bar(first, hash)
+    first.to_s + @baz + hash[:sat].to_s
   end
 end
--- app/controllers/concerns/stringify.rb
+++ app/controllers/concerns/stringify.rb
@@ -7,6 +7,10 @@ class Stringify
   end
 
   def what_am_i
-  	# YOUR IMPLEMENTATION HERE
+    if !name.blank? && !adjective.blank?
+      return name + ' is so ' + adjective
+    else
+      return "You are nothing!"
+    end
   end
 end
